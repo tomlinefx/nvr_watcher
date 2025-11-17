41c1ea6149d334f58e3aab8d004e8801503611f4
tomlinefx <takaha42tom@gmail.com>
2025-11-17 17:24:53 +0900

    feat: ペットテーブルを追加し、ペットの種類と所有者の関係を定義しました

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -5,10 +5,13 @@ import { redirect } from "next/navigation";
 import { auth } from "./auth";
 
 export const verifySession = async () => {
+  // auth.api.getSessionはガチ検証
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  // セッションがなければログインページへ飛ばす！
+  // 変なユーザーはここで弾く！
   if (!session) {
     redirect("/login");
   }
